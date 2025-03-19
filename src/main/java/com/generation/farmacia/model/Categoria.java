a8f1e9043ec095d9251d44bc4a7ecf4bc770230a
Maytê <maytearaujo@yahoo.com.br>
2025-03-19 14:02:43 -0300

    feat: CRUD-produto

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -20,6 +27,10 @@ public class Categoria {
 	@Size(min = 5, max = 100, message = " O atributo nome deve conter no mínimo 10 e no maxímo 100 caracteres")	
 	private String nome;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Produto> produto;
+	
 	public Long getId() {
 		return id;
 	}
@@ -34,6 +45,14 @@ public class Categoria {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 	
 	
---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,96 @@
+package com.generation.farmacia.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	
+	@NotBlank(message = "O Atributo nome é obrigatório")
+	@Size(min = 5, max = 100, message = " O atributo nome deve conter no mínimo 10 e no maxímo 100 caracteres")	
+	public String nome;
+	
+	@NotBlank(message = "O Atributo fabricante é obrigatório")
+	@Size(min = 5, max = 100, message = " O atributo fabricante deve conter no mínimo 10 e no maxímo 100 caracteres")	
+	public String fabricante;
+	
+	@NotNull(message="Data de Validade não pode ser nulo")
+	public LocalDate validade;
+	
+	@NotNull(message="Preço não pode ser nulo")
+	@DecimalMin(value="0.0", inclusive=true,
+	message = "Preço precisa ser maior ou igual a zero")	
+	public double preco;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public LocalDate getValidade() {
+		return validade;
+	}
+
+	public void setValidade(LocalDate validade) {
+		this.validade = validade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+}
